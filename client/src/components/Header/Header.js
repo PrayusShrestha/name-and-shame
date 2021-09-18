c53f13388843c28cf9692803f04fd9925f59f375
Kate Huang <katejhuang@hotmail.com>
2021-09-18 00:46:14 -0700

    create bare home page

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <div className="Header">
+            <h1>Name And Shame</h1>
+            <nav>
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
---
+++ client/src/components/Header/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Search extends React.Component {
+    render() {
+        return (
+            <div className="Search">
+                
+            </div>
+        );
+    }
+}
+
+export default Search;

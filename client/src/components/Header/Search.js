39dc06d4ad7812c840d9256f851a906ef5bbbe3f
Kate Huang <katejhuang@hotmail.com>
2021-09-18 13:03:51 -0700

    add Search functionality

--- client/src/components/Header/Search.js
+++ client/src/components/Header/Search.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AsyncSelect from 'react-select/async';
+import { useHistory } from 'react-router-dom';
 
-class Search extends React.Component {
-    render() {
-        return (
-            <div className="Search">
-                
-            </div>
-        );
-    }
+function Search() {
+    const [query, setQuery] = useState('');
+    const [selected, setSelected] = useState(null);
+    let history = useHistory();
+
+    const handleChange = value => {
+        setSelected(value);
+        history.push("/company");
+    };
+
+    const handleInputChange = value => {
+        setQuery(value);
+    };
+
+    const loadOptions = (query) => {
+        return fetch(process.env.REACT_APP_SERVER_URI + "/search/" + query).then(res => res.json());
+    };
+
+    return (
+        <div className="Search">
+            <AsyncSelect
+                onInputChange={handleInputChange}
+                onChange={handleChange}
+                loadOptions={loadOptions}
+                value={selected}
+                getOptionLabel={e => e.name}
+                getOptionValue={e => e.name}
+            />
+        </div>
+    );
 }
 
 export default Search;

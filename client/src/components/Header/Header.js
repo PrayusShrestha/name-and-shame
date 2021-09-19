b680c4451d0fcb4feb77a4802b34b8e1ba0979d7
aileenrzhang <61919893+aileenrzhang@users.noreply.github.com>
2021-09-18 23:28:16 -0700

    Made title link

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <div className="Header">
-            <h1 id = "site-title">Name And Shame</h1>
+            <a href = "/" id = "site-title"><h1>Name And Shame</h1></a>
             <div id = "search-bar"><Search/></div>
             <Link to="/companies/add">Write a review!</Link>
         </div>

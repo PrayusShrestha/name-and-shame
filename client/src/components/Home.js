f2346285d4ddedac584c964fef27db41dfe0e896
aileenrzhang <61919893+aileenrzhang@users.noreply.github.com>
2021-09-19 03:55:36 -0700

    changed home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,20 +7,12 @@ function Home() {
         <div className="Home">
             <div id = "home-top">
                 <a href = "/"  id = "home-title">Name And Shame</a>
-                <h2 id = "home-caption">A place to expose companies with unethical business practices</h2>
+                <h2 id = "home-caption">Expose companies with unethical business practices</h2>
             </div>
-            <p class = "home-text">Start by:</p>
-            <p class = "home-text">Searching for a company:</p>
-            <div id="home-search"><Search/></div>
-
-            {/* <a href="#" onclick="openPopup(); return false;">Writing a Review</a>
-                <div id="myPopup">
-                <a href="#" id="myExit" onclick="closePopup();return false">x</a>
-                Hello There
-
-                </div> */}
-            <p class = "home-text">Writing a review</p>
-            <Form/>
+            <p className = "home-text">Search for a company</p>
+            <div id="home-search"><Search /></div>
+            <p class = "home-text" id = "home-write-review">Write a review</p>
+            <div id = "home-form"><Form/></div>
         </div>
     );
 }
